9f7eee78a35cae8a3ae220bdc11cf826f0b8e1dc
Jonakoelbl <jonakoelbl@gmail.com>
2014-06-09 18:56:46 -0300

    Modified visual and error visual

--- app/controllers/partidos.rb
+++ app/controllers/partidos.rb
@@ -6,27 +6,29 @@ Macaya::App.controllers :partidos do
 	end
 
 	post :create do
-		torneo = Torneo.get(params[:partido][:torneo])
-		equipo_local = Equipo.get(params[:partido][:equipo_local])
-		equipo_visitante = Equipo.get(params[:partido][:equipo_visitante])
 		
-		unless torneo.nil? 
-			unless equipo_local.eql? equipo_visitante
-				@partido = Partido.new(params[:partido])
-				if @partido.save
-		  			flash[:success] = 'PARTIDO AGREGADO EXITOSAMENTE'
-		  			redirect '/'
-				else
-		  			flash.now[:error] = 'EL PARTIDO NO PUDO SER AGREGADO'
-		  			render 'partidos/new'
+		unless Torneo.get(params[:partido][:torneo]).nil? 
+			unless Equipo.get(params[:partido][:equipo_local]).nil? or Equipo.get(params[:partido][:equipo_visitante]).nil?
+				unless Equipo.get(params[:partido][:equipo_local]).eql? Equipo.get(params[:partido][:equipo_visitante])
+					@partido = Partido.new(params[:partido])
+					if @partido.save
+		  				flash[:success] = 'PARTIDO AGREGADO EXITOSAMENTE'
+		  				redirect '/'
+					else
+		  				flash.now[:error] = 'EL PARTIDO NO PUDO SER AGREGADO'
+		  				render 'partidos/new'
+        			end
+        		else
+        			flash[:error] = 'LOS EQUIPOS DEBEN SER DISTINTOS'
+        			redirect '/partidos/new'	
         		end
         	else
-        		flash.now[:error] = 'LOS EQUIPOS DEBEN SER DISTINTOS'
-        		#render 'partidos/new'
+        		flash[:error] = 'ALGUNOS DE LOS EQUIPOS NO EXISTE'
+        		redirect '/partidos/new'
         	end
         else
-        	flash.now[:error] = 'NO EXISTE TORNEO'
-        	#render 'partidos/new'
+        	flash[:error] = 'NO EXISTE TORNEO'
+        	redirect '/partidos/new'
         end
 	end
 end
