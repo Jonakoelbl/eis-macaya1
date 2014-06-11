d0c075b29883a05f9aae53f828086018e620ef6f
Jonakoelbl <jonakoelbl@gmail.com>
2014-06-11 17:08:04 -0300

    Added combo box to 'agregar partidos' #TODO yet implement cancel button

--- app/controllers/partidos.rb
+++ app/controllers/partidos.rb
@@ -9,26 +9,21 @@ Macaya::App.controllers :partidos do
 
 	post :create do
 		unless Torneo.get(params[:partido][:torneo]).nil? 
-			unless Equipo.get(params[:partido][:equipo_local]).nil? or Equipo.get(params[:partido][:equipo_visitante]).nil?
-				unless Equipo.get(params[:partido][:equipo_local]).eql? Equipo.get(params[:partido][:equipo_visitante])
-					@partido = Partido.new(params[:partido])
-					if @partido.save
-		  				flash[:success] = 'PARTIDO AGREGADO EXITOSAMENTE'
-		  				redirect '/'
-					else
-		  				flash.now[:error] = 'EL PARTIDO NO PUDO SER AGREGADO'
-		  				render 'partidos/new'
-        			end
-        		else
-        			flash[:error] = 'LOS EQUIPOS DEBEN SER DISTINTOS'
-        			redirect '/partidos/new'	
-        		end
-        	else
-        		flash[:error] = 'ALGUNOS DE LOS EQUIPOS NO EXISTE'
-        		redirect '/partidos/new'
+			unless Equipo.get(params[:partido][:equipo_local]).eql? Equipo.get(params[:partido][:equipo_visitante])
+				@partido = Partido.new(params[:partido])
+				if @partido.save
+	  				flash[:success] = 'PARTIDO AGREGADO EXITOSAMENTE'
+	  				redirect '/'
+				else
+	  				flash.now[:error] = 'EL PARTIDO NO PUDO SER AGREGADO'
+	  				render 'partidos/new'
+       			end
+       		else
+        		flash[:error] = 'LOS EQUIPOS DEBEN SER DISTINTOS'
+        		redirect '/partidos/new'	
         	end
         else
-        	flash[:error] = 'NO EXISTE TORNEO'
+        	flash[:error] = 'DEBE SELECCIONAR UN TORNEO'
         	redirect '/partidos/new'
         end
 	end
