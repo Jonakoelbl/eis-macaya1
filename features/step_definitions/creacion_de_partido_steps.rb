15d1bf08fcfb6015a7877dc59a5b017e936dbd2d
David Correa <davidjonatan.c@gmail.com>
2014-06-15 22:59:33 +0200

    Modified features and steps due to changes in the pages view.

--- features/step_definitions/creacion_de_partido_steps.rb
+++ features/step_definitions/creacion_de_partido_steps.rb
@@ -17,26 +17,30 @@ Given(/^que no existan partidos$/) do
 end
 
 When(/^creo un partido para el "(.*?)" con fecha "(.*?)"$/) do |nombre_torneo, fecha|
-  visit '/partidos/new'
-  #click_link ('agregar-partido') #ID del Button
-  fill_in('partido[torneo]', :with => nombre_torneo)
+  click_link (nombre_torneo)
+  click_link ('Agregar Partido') #ID del Button
+  # fill_in('partido[torneo]', :with => nombre_torneo)
   fill_in('partido[fecha]', :with => fecha)
 end
 
 When(/^defino equipo local "(.*?)"$/) do |equipoLocal|
-  fill_in('partido[equipo_local]', :with => equipoLocal)
+  # fill_in('partido[equipo_local]', :with => equipoLocal)  
+  select(equipoLocal, :from => 'equipo_local_select')
 end
 
 When(/^defino equipo visitante "(.*?)"$/) do |equipoVisitante|
-  fill_in('partido[equipo_visitante]', :with => equipoVisitante)  
+  # fill_in('partido[equipo_visitante]', :with => equipoVisitante)  
+  select(equipoVisitante, :from => 'equipo_visitante_select')
 end
 
-Then(/^se visualiza el partido en el fixture$/) do
-  click_button('Agregar')
+When(/^creo el partido$/) do
+  click_button('Crear')
+end
+
+Then(/^se visualiza el partido en el fixture$/) do  
   expect(page).to have_content 'PARTIDO AGREGADO EXITOSAMENTE'
 end
 
 Then(/^muestra un error que el partido ya existe$/) do
-  click_button('Agregar')
   expect(page).to have_content 'EL PARTIDO NO PUDO SER AGREGADO'
 end
