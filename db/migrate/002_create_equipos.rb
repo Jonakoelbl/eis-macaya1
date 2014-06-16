4eba42318c4b880b86d2e8bff3f3cba3e3227278
David Correa <davidjonatan.c@gmail.com>
2014-06-16 02:57:10 +0200

    Added Resultado to Partido and Points to Equipo.

--- db/migrate/002_create_equipos.rb
+++ db/migrate/002_create_equipos.rb
@@ -2,7 +2,8 @@ migration 2, :create_equipos do
   up do
     create_table :equipos do
       column :id, Integer, :serial => true
-      column :name, DataMapper::Property::String, :length => 255,:unique => true#, :key => true
+      column :name, DataMapper::Property::String, :length => 255,:unique => true
+      column :puntaje, DataMapper::Property::Integer
     end
   end
 
